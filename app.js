09b7d0d55dffdba462014fe8da7a32ce92b31860
Kevin Duong <duong.kev@gmail.com>
2016-12-29 21:17:02 -0800

    advanced yargs cmds

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+var titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+};
+var bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions,
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
 var command = argv._[0];
 
 if (command === 'add') {
